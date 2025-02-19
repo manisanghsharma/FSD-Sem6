d89180f591c1d8af97dc5c2f9cf86aef29ef45ce
Manisangh Sharma <manisanghsharma@gmail.com>
2025-02-19 14:55:20 +0530

    server

---
+++ Day 2/MyMath.js
@@ -0,0 +1,13 @@
+const sum = (a, b) => {
+	return a + b;
+};
+
+const mul = (a,b) => {
+    return a*b;
+}
+
+const div = (a,b) => {
+    return a/b
+}
+
+module.exports={sum,mul,div}
---
+++ Day 2/p1.js
@@ -0,0 +1,6 @@
+const obj = require('./MyMath')
+
+console.log(obj.sum(2,3))
+console.log(obj.div(10,2));
+
+
